7d1dae7c8b62d2010c9b4ec1ff4eedc79dbc996f
Lingjie Qiao <katherine_qiao@berkeley.edu>
2016-10-10 10:00:14 -0700

    Update range-value.R

--- test-that/functions/range-value.R
+++ test-that/functions/range-value.R
@@ -1,3 +1,9 @@
-range_value <- function(x) {
-  max(x) - min(x)
+range_value <- function(x, na.rm) {
+  if (na.rm = TRUE) {
+    max <- max(x, na.rm = TRUE)
+    min <- min(x, na.rm = TRUE)
+    max - min
+  } else {
+    max(x) - min(x)
+  }
 }
