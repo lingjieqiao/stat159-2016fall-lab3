ec1a297b8ee4b368aff0a890fedc0cbcd79ab95c
Lingjie Qiao <katherine_qiao@berkeley.edu>
2016-10-10 09:39:31 -0700

    Update better-add.R

---
+++ test-that/functions/better-add.R
@@ -0,0 +1,16 @@
+add <- function(x, y) {
+  if (is.character(x) | is.character(y)) {
+    add_character(x, y)
+  } else{
+    add_numeric(x, y)
+  }
+}
+
+
+add_numeric <- function(x, y) {
+  x + y
+}
+
+add_character <- function(x, y) {
+  paste(x, y)
+}
