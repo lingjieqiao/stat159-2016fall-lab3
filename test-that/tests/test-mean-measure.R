9cf4615ec2c3af3480b4c0feb528c3e879fad7c4
Lingjie Qiao <katherine_qiao@berkeley.edu>
2016-10-10 10:23:30 -0700

    Create test-mean-measure.R

---
+++ test-that/tests/test-mean-measure.R
@@ -0,0 +1,12 @@
+source("../functions/add.R")
+
+context("Measure center value - mean")
+
+test_that("mean as expected", {
+  expect_equal(mean-measure(1, 1), 1)
+  expect_length(mean-measure(1, 1), 1)
+  expect_type(mean-measure(1, 1), 'integer')
+  expect_equal(mean-measure(1, 2, 3, 4, 5), 3)
+  expect_length(mean-measure(1, 2, 3, 4, 5), 1)
+  expect_type(mean-measure(1, 2, 3, 4, 5), 'integer')
+})
